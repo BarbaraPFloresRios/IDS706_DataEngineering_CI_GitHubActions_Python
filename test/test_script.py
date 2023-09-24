7ddf2c029a2fa765f846d41979dedf5d42d81c59
Barbara Flores Rios <143648839+BarbaraPFloresRios@users.noreply.github.com>
2023-09-24 10:23:48 -0400

    Update test_script.py

--- test/test_script.py
+++ test/test_script.py
@@ -14,7 +14,7 @@ def test_aggregate_statistics():
 
 
 def test_github_file_existence():
-    name = "nogibjj" 
+    name = "BarbaraPFloresRios" 
     repo = "IDS706_DataEngineering_BarbaraFlores_Project1"  
     path_to_file = "output/Total_applicants.png"  
     url = f"https://api.github.com/repos/{name}/{repo}/contents/{path_to_file}"
